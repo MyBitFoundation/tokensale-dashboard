3a2a4fe1e33e6a486a6565f375315779e7e1da32
darya.lagun <lagunaby@gmail.com>
2017-02-22 14:00:27 +0300

    finish dashbaoard

--- dist/js/modal.js
+++ dist/js/modal.js
@@ -5,35 +5,10 @@ var Modal = {
     },
 
     initJQueryEvents: function() {
-        $('.js_open_modal').on('click', function(e) {
-            e.preventDefault();
-            Modal.openModal.apply(this);
-        });
 
-        $('.js_dismiss_modal').on('click', function(e) {
-            e.preventDefault();
-            Modal.closeModal.apply(this);
-        });
     },
 
-    openModal: function() {
-        var target = $(this).data('target');
-        $(target).removeClass('none');
-        $('body').append('<div class="modal-backdrop fade"></div>');
-        setTimeout(function() {
-            $('.modal-backdrop').addClass('in');
-            $(target).addClass('in');
-        }, 100)
-    },
-    closeModal: function() {
-        var target = $('.modal.in');
-        $(target).removeClass('in');
-        $('.modal-backdrop').removeClass('in');
-        setTimeout(function() {
-            target.addClass('none');
-            $('.modal-backdrop').remove();
-        }, 100)
-    }
+
 };
 
 $(function() {
