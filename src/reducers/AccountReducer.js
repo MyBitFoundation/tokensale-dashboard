cf357283ba8b71a17bb8b980c4ec64061f8c5f82
Anastasia <anastasyar1996@gmail.com>
2017-02-24 11:39:31 +0300

    fix

--- src/reducers/AccountReducer.js
+++ src/reducers/AccountReducer.js
@@ -9,15 +9,18 @@ const initialState = Map({
 export default function ModalsReducer(state = initialState, action) {
     switch(action.type) {
         case 'INIT_ACCOUNT':
+        console.log(action.payload)
             state = state.set('email', action.payload.email);
             state = state.set('balance', action.payload.balance);
             state = state.set('lastLoginDate', action.payload.lastLoginDate);
             return state;
         case 'LOGOUT':
+        console.log(action.payload)
             state = initialState;
             return state;
         default:
-            state = initialState;
+        console.log(action.payload)
+            //state = initialState;
             return state;
     }
 }
