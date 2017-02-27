e49ab2f8850040a79e5b48d684af22aa235ca7d1
Anastasia <anastasyar1996@gmail.com>
2017-02-27 13:01:53 +0300

    fix token price

--- src/reducers/AccountReducer.js
+++ src/reducers/AccountReducer.js
@@ -3,6 +3,7 @@ import { Map } from 'immutable';
 const initialState = Map({
     email: null,
     balance: 0,
+    tokenPrice: 0,
     lastLoginDate: null,
     address: null,
     tfa: null
@@ -15,7 +16,8 @@ export default function AccountReducer(state = initialState, action) {
             state = state.set('balance', action.payload.balance);
             state = state.set('lastLoginDate', action.payload.lastLoginDate);
             state = state.set('address', action.payload.address);
-            state = state.set('tfa', action.payload.tfa)
+            state = state.set('tfa', action.payload.tfa);
+            state = state.set('tokenPrice', action.payload.tokenPrice);
             return state;
         case 'LOGOUT':
             state = initialState;
