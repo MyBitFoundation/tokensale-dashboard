8a7a2ec8bc8c67bd938ea377fde646e9d25a5da4
Anastasia <anastasyar1996@gmail.com>
2017-03-07 11:27:16 +0300

    fix generation

--- src/actions/ModalsActions.js
+++ src/actions/ModalsActions.js
@@ -23,12 +23,16 @@ class ModalsActions {
                 type: 'MODAL_LOADING'
             });
             post('/crowdsale/deposit', {currency}).then(data => {
-                setTimeout(() => {
-                    dispatch({
-                        type: 'OPEN_STEP_2',
-                        payload: data
-                    })
-                }, 1000)
+                let currency = getState().modals.get('currency');
+
+                if(currency === data.type) {
+                    setTimeout(() => {
+                        dispatch({
+                            type: 'OPEN_STEP_2',
+                            payload: data
+                        })
+                    }, 1000);                    
+                }
             }).catch(error => {
                 setTimeout(() => {
                     dispatch({
