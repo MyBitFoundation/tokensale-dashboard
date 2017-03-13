0abaeede09fb6df9159502d6c1ba157a4eabbba9
Anastasia <anastasyar1996@gmail.com>
2017-03-13 18:22:17 +0300

    change fetch on xhr

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,36 +1,24 @@
-require('es6-promise').polyfill();
-require('isomorphic-fetch');
+// require('es6-promise').polyfill();
+// require('isomorphic-fetch');
 
 export function get(url) {
 	return new Promise((resolve, reject) => {
-		fetch(`${__API_URL__}${url}`, {
-			method: 'GET',
-			headers: new Headers(),
-			cache: 'default',
-			credentials: 'include'
-		}).then(response => {
-			let contentType = response.headers.get("content-type");
-
-			if(response.ok) {
-				if(contentType && contentType.indexOf("application/json") !== -1) {
-					return response.json();
-				} else {
-					return response.text();
-				}
-			} else {
-				throw {code: response.status, message: response.statusText};
+		const xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = () => {
+			if (xhr.readyState == 4 && xhr.status == 200) {
+				resolve(JSON.parse(xhr.responseText));
 			}
-		})
-			.then(data => {
-				resolve(JSON.parse(data))
-			}).catch(error => {
-			reject(error);
-		});
+		};
+		xhr.onerror = () => {
+			reject(JSON.parse(xhr.responseText));
+		}
+		xhr.open('GET', `${__API_URL__}${url}`, true);
+		xhr.withCredentials = true;
+		xhr.send(null);
 	})
 }
 
 export function post(url, params) {
-
 	let details = params;
 	let formBody = [];
 	for(let property in details) {
@@ -39,34 +27,92 @@ export function post(url, params) {
 		formBody.push(encodedKey + "=" + encodedValue);
 	}
 	formBody = formBody.join("&");
-
 	return new Promise((resolve, reject) => {
 
-		fetch(`${__API_URL__}${url}`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded'
-			},
-			credentials: 'include',
-			body: formBody
-		}).then(response => {
-			let contentType = response.headers.get("content-type");
-
-			if(response.ok) {
-				if(contentType && contentType.indexOf("application/json") !== -1) {
-					return response.json();
-				} else {
-					return response.text();
-				}
-			} else {
-				throw {code: response.status, message: response.statusText};
+		const xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = () => {
+			if (xhr.readyState == 4 && xhr.status == 200) {
+				resolve(JSON.parse(xhr.responseText));
 			}
-		})
-			.then(data => {
-				resolve(JSON.parse(data));
-			})
-			.catch((error) => {
-				reject(error);
-			})
+		};
+		xhr.onerror = () => {
+			reject(JSON.parse(xhr.responseText));
+		}
+		xhr.open('POST', `${__API_URL__}${url}`, true);
+		xhr.withCredentials = true;
+		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		xhr.send(formBody);
 	});
 }
+
+
+
+// export function get(url) {
+// 	return new Promise((resolve, reject) => {
+// 		fetch(`${__API_URL__}${url}`, {
+// 			method: 'GET',
+// 			headers: new Headers(),
+// 			cache: 'default',
+// 			credentials: 'include'
+// 		}).then(response => {
+// 			let contentType = response.headers.get("content-type");
+//
+// 			if(response.ok) {
+// 				if(contentType && contentType.indexOf("application/json") !== -1) {
+// 					return response.json();
+// 				} else {
+// 					return response.text();
+// 				}
+// 			} else {
+// 				throw {code: response.status, message: response.statusText};
+// 			}
+// 		})
+// 			.then(data => {
+// 				resolve(JSON.parse(data))
+// 			}).catch(error => {
+// 			reject(error);
+// 		});
+// 	})
+// }
+
+// export function post(url, params) {
+//
+// 	let details = params;
+// 	let formBody = [];
+// 	for(let property in details) {
+// 		let encodedKey = encodeURIComponent(property);
+// 		let encodedValue = encodeURIComponent(details[property]);
+// 		formBody.push(encodedKey + "=" + encodedValue);
+// 	}
+// 	formBody = formBody.join("&");
+//
+// 	return new Promise((resolve, reject) => {
+//
+// 		fetch(`${__API_URL__}${url}`, {
+// 			method: 'POST',
+// 			headers: {
+// 				'Content-Type': 'application/x-www-form-urlencoded'
+// 			},
+// 			credentials: 'include',
+// 			body: formBody
+// 		}).then(response => {
+// 			let contentType = response.headers.get("content-type");
+//
+// 			if(response.ok) {
+// 				if(contentType && contentType.indexOf("application/json") !== -1) {
+// 					return response.json();
+// 				} else {
+// 					return response.text();
+// 				}
+// 			} else {
+// 				throw {code: response.status, message: response.statusText};
+// 			}
+// 		})
+// 			.then(data => {
+// 				resolve(JSON.parse(data));
+// 			})
+// 			.catch((error) => {
+// 				reject(error);
+// 			})
+// 	});
+// }
