10a55fbac20374ebf3bded4c9db6d875eed44e15
Anastasia <anastasyar1996@gmail.com>
2017-03-16 12:48:33 +0300

    use jquery ajax

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,19 +3,18 @@
 
 export function get(url) {
 	return new Promise((resolve, reject) => {
-		const xhr = new XMLHttpRequest();
-		xhr.onreadystatechange = () => {
-			if (xhr.readyState == 4) {
-				if(xhr.status == 200) {
-					resolve(JSON.parse(xhr.responseText));
-				} else {
-					reject({code: xhr.status, response: JSON.parse(xhr.responseText)})
-				}
-			}
-		};
-		xhr.open('GET', `${__API_URL__}${url}`, true);
-		xhr.withCredentials = true;
-		xhr.send(null);
+		$.ajax({
+            type: "GET",
+            dataType: "json",
+            url: `${__API_URL__}${url}`,
+            xhrFields: { withCredentials: true },
+            success: function(res) {
+                resolve(res)
+            },
+            error: function(err) {
+              	reject({code: err.status, response: JSON.parse(err.responseText)})
+            }
+        })
 	})
 }
 
@@ -29,23 +28,67 @@ export function post(url, params) {
 	}
 	formBody = formBody.join("&");
 	return new Promise((resolve, reject) => {
-
-		const xhr = new XMLHttpRequest();
-		xhr.onreadystatechange = () => {
-			if (xhr.readyState == 4) {
-				if(xhr.status == 200) {
-					resolve(JSON.parse(xhr.responseText));
-				} else {
-					reject({code: xhr.status, response: JSON.parse(xhr.responseText)})
-				}
-			}
-		};
-		xhr.open('POST', `${__API_URL__}${url}`, true);
-		xhr.withCredentials = true;
-		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		xhr.send(formBody);
+		$.ajax({
+            type: "POST",
+            url: `${__API_URL__}${url}`,
+            xhrFields: { withCredentials: true },
+            data: formBody,
+            success: function (res) {
+				resolve(res)
+            },
+			error: function(err) {
+              	reject({code: err.status, response: JSON.parse(err.responseText)})
+            }
+        })
 	});
 }
+
+// export function get(url) {
+// 	return new Promise((resolve, reject) => {
+// 		const xhr = new XMLHttpRequest();
+// 		xhr.onreadystatechange = () => {
+// 			if (xhr.readyState == 4) {
+// 				if(xhr.status == 200) {
+// 					resolve(JSON.parse(xhr.responseText));
+// 				} else {
+// 					reject({code: xhr.status, response: JSON.parse(xhr.responseText)})
+// 				}
+// 			}
+// 		};
+// 		xhr.open('GET', `${__API_URL__}${url}`, true);
+// 		xhr.withCredentials = true;
+// 		xhr.setRequestHeader("Content-type","text/json");
+// 		xhr.send(null);
+// 	})
+// }
+//
+// export function post(url, params) {
+// 	let details = params;
+// 	let formBody = [];
+// 	for(let property in details) {
+// 		let encodedKey = encodeURIComponent(property);
+// 		let encodedValue = encodeURIComponent(details[property]);
+// 		formBody.push(encodedKey + "=" + encodedValue);
+// 	}
+// 	formBody = formBody.join("&");
+// 	return new Promise((resolve, reject) => {
+//
+// 		const xhr = new XMLHttpRequest();
+// 		xhr.onreadystatechange = () => {
+// 			if (xhr.readyState == 4) {
+// 				if(xhr.status == 200) {
+// 					resolve(JSON.parse(xhr.responseText));
+// 				} else {
+// 					reject({code: xhr.status, response: JSON.parse(xhr.responseText)})
+// 				}
+// 			}
+// 		};
+// 		xhr.open('POST', `${__API_URL__}${url}`, true);
+// 		xhr.withCredentials = true;
+// 		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+// 		xhr.send(formBody);
+// 	});
+// }
 
 
 
