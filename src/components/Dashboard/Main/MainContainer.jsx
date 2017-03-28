ca345815b1b3aa1f9754cbe1738d057213a3c650
Anastasia <anastasyar1996@gmail.com>
2017-03-28 15:18:03 +0300

    hide block pay with

--- src/components/Dashboard/Main/MainContainer.jsx
+++ src/components/Dashboard/Main/MainContainer.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import Box from './Box';
+// import Box from './Box';
 import History from './History';
 
 export default class Main extends React.Component {
     render() {
         return (
             <div className="main">
-                <Box />
+                {/*<Box />*/}
                 <History />
             </div>
         );
