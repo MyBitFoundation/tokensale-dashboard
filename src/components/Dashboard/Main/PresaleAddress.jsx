7d12272eed8c7c6133c7f29b1ecc37129905d603
Anastasia <anastasyar1996@gmail.com>
2017-06-28 17:41:15 +0300

    add copy button

--- src/components/Dashboard/Main/PresaleAddress.jsx
+++ src/components/Dashboard/Main/PresaleAddress.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {connect} from 'react-redux';
+import copy from 'copy-to-clipboard';
 
 function mapStateToProps(state, ownProps) {
 	return {
@@ -12,18 +13,38 @@ function mapDispatchToProps(dispatch) {
 }
 
 class PresaleAddress extends React.Component {
-	
+
+	constructor() {
+		super();
+		this.state = {
+			copied: false
+		};
+	}
+
+	copy() {
+		copy(this.props.contractAddress);
+		this.setState({copied: true});
+	}
+
 	render() {
 		let {contractAddress} = this.props;
+		let {copied} = this.state;
+
 		return (
 			<div className="address">
 				<div className="address__title">
-						<span className="cur__icon">
-							<img src="images/eth.png" alt="" />
-						</span>
+					<span className="cur__icon">
+						<img src="images/eth.png" alt=""  />
+					</span>
 					<span className="cur__text">ETH Address:</span>
 				</div>
-				<div className="address__text">{contractAddress}</div>
+				<button className={`address__btn ${copied ? 'active' : ''}`} type="button" onClick={() => this.copy()}>
+					<span className="btnText">{copied ? 'Copied' : 'Copy Address'} </span>
+					{copied ? <span className="btnIcon icon-check"></span> : null}
+				</button>
+				<div className="address__fieldWrap">
+					<input type="text" readOnly className="address__field" value={contractAddress} />
+				</div>
 			</div>
 		)
 	}
