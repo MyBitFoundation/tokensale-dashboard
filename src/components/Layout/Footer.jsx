53e16a6bac8ffa37109b3e52ed57c50d58502095
Sergey Furs <itscorpeon@gmail.com>
2017-08-14 10:39:54 +0300

    remove link in the footer

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -12,12 +12,6 @@ export default class Footer extends React.Component {
                         © 2017 My Bit.   <Translate content="footer.rights"/>
                     </div>
                     <div className="footer__linkWrap">
-        				<Link
-                            to="https://gist.github.com/MyBitDev/4bbd6aec6904a6c21eb47c3179287078"
-                            className="footer__link"
-                            target="_blank"
-                        ><Translate content="footer.code"/></Link>
-                      <Link to="/MyBit_Deal_Sheet.pdf" className="footer__link" target="_blank"><Translate content="footer.deel"/></Link>
         				<Link to="https://mybit.io#contact" className="footer__link" target="_blank"><Translate content="footer.contact"/></Link>
         			</div>
                 </div>
